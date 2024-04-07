cb4349bd33ac538f1143ef4a26e0c0d784cb353d
adelina010416 <adelinazima@mail.ru>
2024-04-07 21:55:28 +0400

    fixed authorization

--- users/serializer.py
+++ users/serializer.py
@@ -5,8 +5,9 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    payment = PaymentSerializer(source='payment_set', many=True)
+    payment = PaymentSerializer(source='payment_set', read_only=True, many=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'city', 'phone', 'avatar', 'payment']
+        fields = ['email', 'city', 'phone', 'avatar', 'payment', 'password']
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import User
 from users.serializer import UserSerializer
@@ -7,4 +8,11 @@ from users.serializer import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
 
